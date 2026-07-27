974a88adb30098c3662e1efb9abc22d6f24e26cb
riteshkharat07 <riteshkharat07@gmail.com>
2026-07-27 20:19:30 +0530

    Create Student.java

---
+++ Day-04-GetMapping/Student.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String city;
+
+    public Student(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
