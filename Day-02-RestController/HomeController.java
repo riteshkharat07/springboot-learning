56963433afa5db5110f0bcaac41a10afcb3e4a5c
riteshkharat07 <riteshkharat07@gmail.com>
2026-07-27 20:13:31 +0530

    Day 02 - Created first REST API using @RestController

---
+++ Day-02-RestController/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+
+        return "Welcome to Spring Boot!";
+
+    }
+
+}
