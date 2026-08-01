14029d7e4ac03dff0367d9ba0f853eff748b7dc2
riteshkharat07 <riteshkharat07@gmail.com>
2026-08-01 13:37:32 +0530

    Create StudentRepository.java

---
+++ Day-11-Dependency-Injection/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+
+    public String getStudent() {
+        return "Student Data From Repository";
+    }
+
+}
