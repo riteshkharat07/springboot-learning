29e141f11b14441212a508ca43aeea4de1ec9928
riteshkharat07 <riteshkharat07@gmail.com>
2026-08-01 13:37:08 +0530

    Create StudentService.java

---
+++ Day-11-Dependency-Injection/StudentService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public String getStudent() {
+        return studentRepository.getStudent();
+    }
+
+}
