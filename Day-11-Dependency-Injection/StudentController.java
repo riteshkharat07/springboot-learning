241c91f888b2534557ba55dfb46ffb8d93e9b2c9
riteshkharat07 <riteshkharat07@gmail.com>
2026-08-01 13:36:46 +0530

    Create StudentController.java

---
+++ Day-11-Dependency-Injection/StudentController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/student")
+    public String getStudent() {
+        return studentService.getStudent();
+    }
+
+}
