78c0626650097ceb8075d1ec2573e1593b3e2433
riteshkharat07 <riteshkharat07@gmail.com>
2026-08-06 12:49:49 +0530

    Create StudentService.java

---
+++ Day-18-Update-Student/StudentService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student updateStudent(int id, Student student){
+
+        student.setId(id);
+
+        return studentRepository.save(student);
+
+    }
+
+}
