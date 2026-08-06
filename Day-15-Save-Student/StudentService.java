951433b9fd3a21bf374f1bdb3ffd281f10e67d7e
riteshkharat07 <riteshkharat07@gmail.com>
2026-08-06 12:31:52 +0530

    Create StudentService.java

---
+++ Day-15-Save-Student/StudentService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public void saveStudent(Student student){
+
+        studentRepository.save(student);
+
+    }
+
+}
