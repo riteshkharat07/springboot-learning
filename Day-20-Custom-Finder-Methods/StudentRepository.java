1f4f6dbb313af5c54c4223c6cf131450f91f725c
riteshkharat07 <riteshkharat07@gmail.com>
2026-08-07 19:10:48 +0530

    Create StudentRepository.java

---
+++ Day-20-Custom-Finder-Methods/StudentRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    List<Student> findByName(String name);
+
+    List<Student> findByCity(String city);
+
+    List<Student> findByNameAndCity(String name, String city);
+
+}
