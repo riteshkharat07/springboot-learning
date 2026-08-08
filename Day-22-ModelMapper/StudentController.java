9477867c65f3a7b4efc1e079655e070c1490ea57
riteshkharat07 <riteshkharat07@gmail.com>
2026-08-08 15:57:34 +0530

    Create StudentController.java

---
+++ Day-22-ModelMapper/StudentController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.StudentDTO;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public StudentDTO saveStudent(@RequestBody StudentDTO studentDTO) {
+
+        return studentService.saveStudent(studentDTO);
+    }
+}
