8085f968f9824f934ba04f563d1994d830dc4a74
riteshkharat07 <riteshkharat07@gmail.com>
2026-08-10 19:56:41 +0530

    Create StudentController.java

---
+++ Day-24-Global-Exception-Handling/StudentController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.StudentDTO;
+import com.example.demo.service.StudentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public StudentDTO saveStudent(
+            @Valid @RequestBody StudentDTO studentDTO) {
+
+        return studentService.saveStudent(studentDTO);
+    }
+}
