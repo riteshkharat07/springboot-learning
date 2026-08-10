de9592f44291b3c0594198d0e5abad563628377b
riteshkharat07 <riteshkharat07@gmail.com>
2026-08-10 19:56:16 +0530

    Create StudentDTO.java

---
+++ Day-24-Global-Exception-Handling/StudentDTO.java
@@ -0,0 +1,52 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class StudentDTO {
+
+    private int id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50,
+            message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @NotBlank(message = "City is required")
+    @Size(min = 2, max = 50,
+            message = "City must be between 2 and 50 characters")
+    private String city;
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
