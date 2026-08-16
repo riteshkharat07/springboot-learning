925758698625f8fbaa9d8e5680a6ebb4f4111fad
riteshkharat07 <riteshkharat07@gmail.com>
2026-08-16 13:00:21 +0530

    Create StudentController.java

---
+++ Day-28-Search-API/StudentController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/search")
+    public List<Student> searchStudent(
+            @RequestParam String name) {
+
+        return studentService.searchStudent(name);
+    }
+}
